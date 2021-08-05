925d4fd3da7ce8598931629ba42692cbb5d8ca6c
Alen Thankachan <alenthankz@gmail.com>
2021-08-05 21:24:18 +0530

    modified

--- src/controllerCmdBased.js
+++ src/controllerCmdBased.js
@@ -1,17 +1,17 @@
 const parkingLotCreater = require('./ParkingLotCreator')
 
-function controllerCmdBased(data, parkingLot){
+function controllerCmdBased(data, obj){
 
     data = data.replace(/[\r\n]/g, "")
     let arr = data.split(' ');
     switch ( arr[0] ){
-        case 'create_parking_lot' : {  parkingLot = new parkingLotCreater(Number(arr[1]));  break;}
-        case 'park':{ parkingLot.park(arr[1],arr[2]); break;}
-        case 'leave':{ parkingLot.leave( Number(arr[1]) ); break;}
-        case 'status':{ parkingLot.status( ); break;}
-        case 'registration_numbers_for_cars_with_colour':{ parkingLot.colorBasedSelectionRegist(arr[1]); break;}
-        case 'slot_numbers_for_cars_with_colour':{ parkingLot.colorBasedSelectionSlot(arr[1]); break;}
-        case 'slot_number_for_registration_number':{ parkingLot.registNoBasedSelection(arr[1]); break;}
+        case 'create_parking_lot' : {  obj['parkingLot'] = new parkingLotCreater(Number(arr[1]));  break;}
+        case 'park':{ obj.parkingLot.park(arr[1],arr[2]); break;}
+        case 'leave':{ obj.parkingLot.leave( Number(arr[1]) ); break;}
+        case 'status':{ obj.parkingLot.status( ); break;}
+        case 'registration_numbers_for_cars_with_colour':{ obj.parkingLot.colorBasedSelectionRegist(arr[1]); break;}
+        case 'slot_numbers_for_cars_with_colour':{ obj.parkingLot.colorBasedSelectionSlot(arr[1]); break;}
+        case 'slot_number_for_registration_number':{ obj.parkingLot.registNoBasedSelection(arr[1]); break;}
         default:break;
     }
 
